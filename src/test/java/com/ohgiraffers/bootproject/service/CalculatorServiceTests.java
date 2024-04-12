7f873d419be965a704e3d8b3286b70498088715d
t4e1 <jtw5738@naver.com>
2024-04-12 09:43:11 +0900

    테스트 2

--- src/test/java/com/ohgiraffers/bootproject/service/CalculatorServiceTests.java
+++ src/test/java/com/ohgiraffers/bootproject/service/CalculatorServiceTests.java
@@ -28,7 +28,7 @@ class CalculatorServiceTests {
                 Arguments.of(new CalculatorDTO(200, -103))
         );
     }
-
+    
     @DisplayName("두 수의 합 구하기 테스트")
     @ParameterizedTest
     @MethodSource("provideDTOSource")
